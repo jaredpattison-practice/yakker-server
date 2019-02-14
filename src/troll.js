ccb5636001f0ee1dbf5ef3377c6685c5c4121dde
jared <jaredpattison@users.noreply.github.com>
2019-02-13 20:32:03 -0800

    message history (15) working

--- src/troll.js
+++ src/troll.js
@@ -1,38 +1,50 @@
 import React from 'react';
-import io from 'socket.io-client';
+import ClientConnection from './lib/subscriber.js';
+import Publisher from './lib/publisher.js';
 
-// const url = 'http://localhost:3000';
-const url = 'https://js-401-socket-io-server.herokuapp.com';
-const socket = io.connect(url);
+const Q = new Publisher();
+const TrollClient = new ClientConnection('troll');
 
-class TrollJohn extends React.Component {
+
+class Troll extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       typedInput: '',
-      words: '',
+      words: [],
     };
-
-    socket.on('incoming', payload => this.updateWords(payload));
+  
+    TrollClient.subscribe('message', payload => {
+      this.updateWords(payload);
+    })
   }
 
-  updateWords = words => {
-    this.setState({ words });
+  updateWords = word => {
+    if(this.state.words.length >= 15) {
+      this.state.words.shift();
+    }
+    this.setState({words: [ ...this.state.words, word]});
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    socket.emit('troll', this.state.typedInput);
+    Q.publish('troll', 'message', this.state.typedInput)
   };
 
   handleNewWords = event => {
+    event.preventDefault();
     this.setState({ typedInput: event.target.value });
   };
 
   render() {
+    const listItem = this.state.words.map((word, idx) =>
+    <li key={idx}>{word}</li>
+    );
+
     return (
       <>
-        <h2>{this.state.words}</h2>
+        <ul>{listItem}</ul>
+        <h2>{this.state.words[this.state.words.length - 1]}</h2>
         <form onSubmit={this.handleSubmit}>
           <input
             name="typedInput"
@@ -45,4 +57,6 @@ class TrollJohn extends React.Component {
   }
 }
 
-export default TrollJohn;
+
+
+export default Troll;
